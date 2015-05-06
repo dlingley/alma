773ce218e296c58a34bba18f5f93aa556f8f30d4
Matt Johnson <mdj2@pdx.edu>
2015-05-06 11:13:09 -0700

    Use the API to get the items; Added some other API snippets

--- alma/alma/api.py
+++ alma/alma/api.py
@@ -1,6 +1,9 @@
 import requests
 import json
+import time
 from functools import partial
+import xml.etree.ElementTree as ET
+#from alma.items.models import Item
 
 API_KEY = "l7xx12b2a3ae884d4b48a5de50b3f8539fa6"
 
@@ -14,10 +17,12 @@ class AlmaError(Exception):
     def __repr__(self):
         return str(self)
 
+
 def request(endpoint, params=None, data=None, method="get"):
     if params is None:
         params = {}
 
+    headers = {}
     params.update({
         "format": "json",
         "apikey": API_KEY
@@ -27,8 +32,12 @@ def request(endpoint, params=None, data=None, method="get"):
         method = requests.get
     elif method == "post":
         method = requests.post
+        headers['content-type'] = "application/json"
+        if data:
+            data = json.dumps(data)
 
-    response = method("https://api-na.hosted.exlibrisgroup.com/" + endpoint, params=params, data=data)
+
+    response = method("https://api-na.hosted.exlibrisgroup.com/" + endpoint, params=params, data=data, headers=headers)
 
     try:
         content = json.loads(response.content.decode())
@@ -41,32 +50,90 @@ def request(endpoint, params=None, data=None, method="get"):
 
     return content
 
+
 get = partial(request, method="get")
 post = partial(request, method="post")
+
 
 def get_users():
     return get("almaws/v1/users", {"limit": 100, "offset": 0, "order_by": "primary_id"})
 
+
 def get_user(identifer):
     return get('almaws/v1/users/' + str(identifer))
+
 
 def get_libraries():
     return get("almaws/v1/conf/libraries")
 
+
 def get_locations(library_code):
     return get("almaws/v1/conf/libraries/{0}/locations".format(library_code))
 
+
 def get_codes(code):
     return get("almaws/v1/conf/code-tables/" + code)
+
+
+def get_items():
+    """
+    Returns a list of dicts that represent items in Alma
+    WARNING: This only gets the first 1000 rows from the report
+    """
+    data = {
+        "path": "/shared/Portland State University/Reports/cg oit avs",
+        "limit": 1000
+    }
+
+    while True:
+        response = get("almaws/v1/analytics/reports", data)
+        xml = ET.fromstring(response['anies'][0])
+        if xml.findall("./IsFinished")[0].text == "false":
+            data['token'] = xml.findall("./ResumptionToken")[0]
+            time.sleep(1)
+        else:
+            break
+
+    #xml = ET.fromstring(open("xml.txt", "r").read())
+    col_names = ["useless", "mms_id", "name", "library_code", "barcode", "description", "item_id", "category"]
+    rows = []
+    for i, row in enumerate(xml.findall(".//{urn:schemas-microsoft-com:xml-analysis:rowset}Row")):
+        item = {}
+        for col in row:
+            # the tag name ends with a number (e.g. Column5), which we use to get the column
+            # name
+            col_index = int(col.tag[-1])
+            item[col_names[col_index]] = col.text
+        rows.append(item)
+
+    return rows
+
+
+def update_items():
+    for item in get_items():
+        try:
+            model_obj = Item.objects.get(pk=item['item_id'])
+        except Item.DoesNotExist:
+            model_obj = Item(pk=item['item_id'])
+
+        col_names = ["mms_id", "name", "library_code", "barcode", "description", "category"]
+        for col_name in col_names:
+            try:
+                setattr(model_obj, col_name, item[col_name])
+            except KeyError:
+                pass
+
+        model_obj.save()
+
 
 library_code = "AVS"
 location_code = "OITAVS"
 id_type = "UNIV_ID"
 
-def create_booking(username, barcode, start_date, end_date):
+def create_booking(username, item_id, start_date, end_date):
     params = {
-        "item_barcode": barcode,
-        #"user_id_type": id_type,
+        "item_pid": item_id,
+        "user_id_type": id_type,
     }
 
     return post("almaws/v1/users/%s/requests" % username, params=params, data={
@@ -89,8 +156,10 @@ def create_booking(username, barcode, start_date, end_date):
 #    "booking_end_date": now(),
 #}
 
-print(json.dumps(get_users(), indent=4))
+#print(json.dumps(get_users(), indent=4))
 
 #print(json.dumps(get_user("mdj2"), indent=4))
 #print(json.dumps(get_codes("UserIdentifierTypes"), indent=4))
-print(json.dumps(create_booking("200035", 123, "2015-05-05T10:10:10Z", "2015-05-05T11:10:10Z")))
+#print(json.dumps(create_booking("200035", 123, "2015-05-05T10:10:10Z", "2015-05-05T11:10:10Z")))
+#print(json.dumps(get_report("/shared/Portland State University/Reports/cg oit avs"), indent=4))
+print(json.dumps(create_booking("mdj2", "23305517810001853", "2015-05-06T10:10:10Z", "2015-05-06T11:10:10Z")))
